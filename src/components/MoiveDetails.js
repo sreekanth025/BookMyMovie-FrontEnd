40de63e42320cad5e86a728b1ed714303a0afc0d
sreekanth025 <vs25@iitbbs.ac.in>
2021-05-01 00:18:00 +0530

    dynamic cast&crew images

--- src/components/MoiveDetails.js
+++ src/components/MoiveDetails.js
@@ -81,7 +81,8 @@ const MovieBanner = ({ data }) => {
           {data.crew.map((item, indx) => (
             <div className="text-center" key={indx}>
               <img
-                src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF6mVep8mmwTbVD9A6_0tAkjhiPOUikhenZnH3NGEImOX4eW2pbXJ8E-WmJcWXjcXY-bA&usqp=CAU`}
+                // src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF6mVep8mmwTbVD9A6_0tAkjhiPOUikhenZnH3NGEImOX4eW2pbXJ8E-WmJcWXjcXY-bA&usqp=CAU`}
+                src={item.image_link}
                 alt={"profile default"}
                 className="rounded-circle"
                 width="100"
